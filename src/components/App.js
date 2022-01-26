bdced934f1c9bd3f43817eff9a0637fdac91ddf5
Michael Harris <xero5k@yahoo.com>
2022-01-25 19:45:06 -0600

    map of gryffindor students

--- src/components/App.js
+++ src/components/App.js
@@ -9,20 +9,42 @@ import Home from './Home';
 function App() {
 
   const [students, setStudents] = useState([]);
-
+  const [gryffStudents, setGryffStudents] = useState ([]);
+  //Make gryff state above here
   useEffect(() => {
     fetch("http://localhost:9292/students")
     .then((r) => r.json())
     .then((student) => setStudents(student))
   }, []);
   
+  // To get the students separated in each house:
+  // Copy Students only with the same house ids
+
+  /**** Gryffindor Students */
+  useEffect(() => {
+    fetch("http://localhost:9292/gryffindor")
+    .then((r) => r.json())
+    .then((student) => setGryffStudents(student))
+  }, []);
+  console.log("(A)Gryff Students: ", gryffStudents)
+  const gryff_student_id = gryffStudents.map((student) => {
+    return student.id
+  })
+  const gryff_student_name = gryffStudents.map((student) => {
+    return student.name
+  })
+  /******** */
+
+
+  /**** All Students */
   const student_id = students.map((student) => {
     return student.id
   })
-  
   const student_name = students.map((student) => {
     return student.name
   })
+ /******** */
+
   console.log("(A)Student names: ", student_name)
   console.log(students)
 
